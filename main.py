d2a13d979de3d7fae83712098ee6a95bf7f0cdf0
lutje <luetje.helzel@gmail.com>
2025-07-09 15:18:54 +0200

    autoformatting

--- main.py
+++ main.py
@@ -182,9 +182,7 @@ def inject_current_user():
 
 
 if __name__ == "__main__":
-    # Initialize DB and create example users with hashed passwords
     with app.app_context():
-
         db.create_all()
 
     app.run(host="0.0.0.0", debug=True, port=81)
